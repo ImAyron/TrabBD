852b38d4d0b957294c34e98a159e18b69d99130c
LucasBrazza <lucas.brazza.lb@gmail.com>
2021-12-28 02:57:30 -0300

    add some querys

---
+++ teste/User/querys.php
@@ -0,0 +1,74 @@
+<?php
+
+function filtroTodasAsUsuarios()
+{
+    require '../dbc/index.php';
+
+    $stmt = $pdo->query("SELECT * FROM USUARIOS ORDER BY email");
+
+    return $stmt->fetchAll();
+}
+
+function inserirUsuarios($email, $cargo)
+{
+    require '../dbc/index.php';
+
+    try {
+
+        $pdo->beginTransaction();
+
+        $stmt = $pdo->prepare("INSERT INTO USUARIOS (email, cargo) VALUES (:email, :cargo)");
+
+        $stmt->bindParam(":email", $email);
+        $stmt->bindParam(":cargo", $cargo);
+
+        $stmt->execute();
+
+        $pdo->commit();
+    } catch (Exception $error) {
+        $pdo->rollBack();
+        die("Erro na inserção! " . $error->getMessage());
+    }
+}
+
+function editarUsuario($email, $cargo)
+{
+    require '../dbc/index.php';
+
+    try {
+
+        $pdo->beginTransaction();
+
+        $stmt = $pdo->prepare("UPDATE USUARIOS SET cargo=:cargo WHERE email=:email");
+        
+        $stmt->bindParam(":email", $email);
+        $stmt->bindParam(":cargo", $cargo);
+
+        $stmt->execute();
+
+        $pdo->commit();
+    } catch (Exception $error) {
+        $pdo->rollBack();
+        die("Erro! " . $error->getMessage());
+    }
+}
+
+function deletarUsuario($email)
+{
+    require '../dbc/index.php';
+
+    try {
+
+        $pdo->beginTransaction();
+
+        $stmt = $pdo->prepare("DELETE FROM USUARIOS WHERE email=:email");
+
+        $stmt->bindParam(":email", $email);
+        $stmt->execute();
+
+        $pdo->commit();
+    } catch (Exception $error) {
+        $pdo->rollBack();
+        die("Erro ao deletar! " . $error->getMessage());
+    }
+}
